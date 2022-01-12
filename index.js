1329de9f08d71452b0111a3692930b454475876b
vinicius-batista <20201012000256@pucgo.edu.br>
2022-01-11 21:29:01 -0300

    Configurando o pagamento

--- index.js
+++ index.js
@@ -13,15 +13,39 @@ app.get("/", (req, res) => {
     res.send("Ola mundo" + Date.now());
 })
 
-app.get("/pagar", (req, res) => {
-   
+
+app.get("/pagar", async (req, res) => {
+
+    
+
+    var id = "" + Date.now();
+    var emailDoPagador = "viniciusbbf3@hotmail.com";
+
     var dados = {
         items: [
             item = {
-                id: Date.now()
+                id: " " + Date.now(),
+                description: "2x video game; 3x camisas",
+                quntity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(150)
             }
-        ]
+        ],
+        //Na hora que o pagamento vai ser gerado no banco
+        payer: {
+            email: "viniciusbbf3@hotmail.com"
+        },
+        external_reference: id
     }
+    try{
+        var pagamento = await MercadoPago.preferences.create(dados);
+        console.log(pagamento);
+        return res.redirect(pagamento.body.init_point);
+
+    }catch(err){
+        return res.send(err.message);
+    }
+  
 });
 
 app.listen(PORT, (req, res) => {
